f3dc40bfed8d6289dd31852d7b405bb07c5002ea
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2023-11-08 22:09:33 -0500

    Video 13

--- 08-formularios/src/app/pages/template/template.component.ts
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from 'src/app/services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -8,21 +9,25 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateComponent implements OnInit {
 
-  usuario={
+  usuario = {
     nombre: 'Kevin',
     apellido:'Chiquil',
     correo:'kevin@gmail.com'
-
   }
 
-  constructor( ) {}
+  constructor( private paisService: PaisService ) {}
+
   ngOnInit():void{
+
+    this.paisService.getPaises()
+      .subscribe( paises =>{
+        console.log(paises);
+      })
 
   }
 
   guardar( forma: NgForm ){
     console.log(forma);
-    console.log(forma.value);
 
     if( forma.invalid){
       Object.values( forma.controls).forEach(control =>{

439955ead6a416195d5607c77e78bc371beefa82
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2023-11-08 22:22:51 -0500

    Video 14

--- 08-formularios/src/app/pages/template/template.component.ts
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -12,8 +12,11 @@ export class TemplateComponent implements OnInit {
   usuario = {
     nombre: 'Kevin',
     apellido:'Chiquil',
-    correo:'kevin@gmail.com'
+    correo:'kevin@gmail.com',
+    pais: ''
   }
+
+  paises: any[] = [];
 
   constructor( private paisService: PaisService ) {}
 
@@ -21,7 +24,14 @@ export class TemplateComponent implements OnInit {
 
     this.paisService.getPaises()
       .subscribe( paises =>{
-        console.log(paises);
+        this.paises = paises;
+
+        this.paises.unshift({
+          nombre: '[Seleccione Pais]',
+          codigo: ''
+        })
+
+        console.log( this.paises );
       })
 
   }
